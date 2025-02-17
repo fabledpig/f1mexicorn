702cacebfb3d378f065173e188e2634575f8a954
sirthomas96 <sirthom96@gmail.com>
2025-02-18 00:36:00 +0100

    backend:fix result schema

--- backend/app/models/sql_models.py
+++ backend/app/models/sql_models.py
@@ -51,9 +51,10 @@ class Guess(SQLModel, table=True):
 
 
 class RaceResult(SQLModel, table=True):
-    race_result_id: Optional[int] = Field(default=None, primary_key=True)
     race_id: int = Field(
-        sa_column=Column(Integer, ForeignKey("race.race_id", ondelete="CASCADE"))
+        sa_column=Column(
+            Integer, ForeignKey("race.race_id", ondelete="CASCADE"), primary_key=True
+        )
     )
     first_place_driver_number: int
     second_place_driver_number: int
