2480fbbc8d7a66d0c381c22ac8a2edad4db79882
sirthomas96 <sirthom96@gmail.com>
2025-10-25 19:38:42 +0200

    Add some css

--- frontend_angular/f1mexicorn-app/src/app/race-drivers/race-drivers.component.ts
+++ frontend_angular/f1mexicorn-app/src/app/race-drivers/race-drivers.component.ts
@@ -8,6 +8,20 @@ import { SseService } from '../services/sse.service';
 import { Observable } from 'rxjs/internal/Observable';
 import { Subscription } from 'rxjs';
 
+const teamLogos = new Map<string, string>([
+  ['Mercedes', "url('/assets/teams/mercedes.png')"],
+  ['Red Bull Racing', "url('/assets/teams/redbull.png')"],
+  ['Ferrari', "url('/assets/teams/ferrari.png')"],
+  ['McLaren', "url('/assets/teams/mclaren.png')"],
+  ['Alpine', "url('/assets/teams/alpine.png')"],
+  ['Aston Martin', "url('/assets/teams/aston.png')"],
+  ['Williams', "url('/assets/teams/williams.png')"],
+  ['Racing Bulls', "url('/assets/teams/visarb.png')"],
+  ['Haas F1 Team', "url('/assets/teams/haas.png')"],
+  ['Kick Sauber', "url('/assets/teams/stake.png')"]
+]);
+
+
 @Component({
   selector: 'app-race-drivers',
   templateUrl: './race-drivers.component.html',
@@ -33,20 +47,25 @@ export class RaceDriversComponent {
         this.race = race[0];
         this.eventFinished = this.isNextEventFinished(this.race);
         console.log("Latest race:", this.race);
-        // Return the second API call
         console.log("Fetching drivers for raceId:", this.race.race_id);
         return this.apiService.getSessionDrivers(this.race.race_id ?? 0);
-      })
-    ).subscribe({
-      next: (drivers: Driver[]) => {
+      }),
+      switchMap((drivers: Driver[]) => {
         this.drivers = drivers;
         console.log("Drivers in latest race:", drivers);
+        return this.apiService.getGuess();
+      })).subscribe({
+      next: (guess: Guess) => {
+        this.populatePrediction(guess);
+        console.log("User's existing guess:", guess);
       },
       error: (err) => {
         console.error("Error fetching data:", err);
       }
     });
 
+    // Set up SSE connection
+    // TODO CHANGE THIS AFTER FIGURING OUT WHAT TO DO HERE LOL
     this.message$ = this.sseService.connect();
     this.sseSubscription = this.message$.subscribe({
       next: (message: string) => {
@@ -118,6 +137,40 @@ export class RaceDriversComponent {
     return raceDate < now;
   }
 
+  populatePrediction(guess: Guess) {
+    // populate prediction array based on guess
+    this.prediction = [];
+    if (guess.position_1_driver_id) {
+      const driver1 = this.drivers.find(d => d.driver_number === guess.position_1_driver_id);
+      if (driver1) {
+        this.prediction.push(driver1);
+        this.drivers = this.drivers.filter(d => d.driver_number !== driver1.driver_number);
+      }
+    }
+    if (guess.position_2_driver_id) {
+      const driver2 = this.drivers.find(d => d.driver_number === guess.position_2_driver_id);
+      if (driver2) {
+        this.prediction.push(driver2);
+        this.drivers = this.drivers.filter(d => d.driver_number !== driver2.driver_number);
+      }
+
+    }
+    if (guess.position_3_driver_id) {
+      const driver3 = this.drivers.find(d => d.driver_number === guess.position_3_driver_id);
+      if (driver3) {
+        this.prediction.push(driver3);
+        this.drivers = this.drivers.filter(d => d.driver_number !== driver3.driver_number);
+      }
+    }
+  }
+
+  getDriverBackground(driver: Driver): { [key: string]: string } {
+    return {
+      'background-image': teamLogos.get(driver.team) || 'none',
+      'background-size': 'fit',
+      'background-position': 'center',
+      'background-color': 'rgba(183, 208, 216, 1)',
+      'background-blend-mode': 'overlay'
+    }
+  }
 }
-
-
